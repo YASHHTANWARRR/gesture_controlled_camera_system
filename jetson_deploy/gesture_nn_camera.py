9c0d4d1ed44c7477704acda1e308c0990a056914
YASHHTANWARRR <yashtanwar11092005@gmail.com>
2026-04-29 16:03:48 +0530

    done

--- jetson_deploy/gesture_nn_camera.py
+++ jetson_deploy/gesture_nn_camera.py
@@ -21,7 +21,7 @@ mp_draw = mp.solutions.drawing_utils
 
 hands = mp_hands.Hands(
     max_num_hands=1,
-    model_complexity=0,   # 🔥 faster
+    model_complexity=0,  
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
